790da4cecc7fbc793715aa0c56d479a8a4a111e6
AntonAlgo <133565476+AntonAlgo@users.noreply.github.com>
2023-05-21 12:16:00 +0300

    Update ping-pong.py
    конечная версия

--- project/ping-pong.py
+++ project/ping-pong.py
@@ -1,9 +1,9 @@
 from pygame import *
+from random import *
 
 
 class GamsSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, 
-    player_speed, wight, height):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
         super().__init__()
         self.image=transform.scale(image.load(player_image),(wight,height))
         self.speed = player_speed
@@ -60,9 +60,30 @@ while game:
         window.fill(back)
         racket1.update_l()
         racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= -1 - random()
+            #ball.image = transform.rotate(ball.image ,randint(-180, 180))
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = False
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = False
+
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
 
         racket1.reset()
         racket2.reset()
         ball.reset()
+
     display.update()
     clock.tick(FPS)
